12b0890f092378aafabf22f26b50c85b273ad956
Sam Elliott <samde@microsoft.com>
2022-12-22 20:56:02 -0400

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+function Home() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
-export default HomePage;
+export default Home;

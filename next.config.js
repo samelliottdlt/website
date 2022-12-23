b42ec14fc9c49accb91645028ba3d7d14352ef48
Sam Elliott <samde@microsoft.com>
2022-12-22 20:42:05 -0400

    remove and fix rewrites

--- next.config.js
+++ next.config.js
@@ -9,28 +9,6 @@ module.exports = {
       {
         source: "/beluga/:slug",
         destination:
-          "https://sams-awesome-space.nyc3.digitaloceanspaces.com/:slug",
-      },
-      {
-        source: "/",
-        has: [
-          {
-            type: "host",
-            value: "beluga.sdelatorrebaba.com",
-          },
-        ],
-        destination:
-          "https://sams-awesome-space.nyc3.digitaloceanspaces.com/index.html",
-      },
-      {
-        source: "/:slug",
-        has: [
-          {
-            type: "host",
-            value: "beluga.sdelatorrebaba.com",
-          },
-        ],
-        destination:
           "https://sams-awesome-space.nyc3.digitaloceanspaces.com/index.html/:slug",
       },
     ];

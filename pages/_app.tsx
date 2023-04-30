ec9e68f7ccfceb7825513c141f748e8b399cd878
Sam Elliott <samde@microsoft.com>
2023-04-30 17:25:51 -0400

    migrate to next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import Navbar from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });

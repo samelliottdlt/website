baba74365337e8bca579dc02545a3a6b7415131a
Sam Elliott <samde@microsoft.com>
2023-07-25 16:08:51 -0400

    prettier:fix

--- public/gbstudio/script.js
+++ public/gbstudio/script.js
@@ -6,7 +6,7 @@
  */
 "use strict";
 
-const customControls = {}
+const customControls = {};
 
 // User configurable.
 const ROM_FILENAME = "gbstudio/game.gb";
@@ -170,14 +170,14 @@ class Emulator {
     this.module = module;
     this.romDataPtr = this.module._malloc(romBuffer.byteLength);
     makeWasmBuffer(this.module, this.romDataPtr, romBuffer.byteLength).set(
-      new Uint8Array(romBuffer)
+      new Uint8Array(romBuffer),
     );
     this.e = this.module._emulator_new_simple(
       this.romDataPtr,
       romBuffer.byteLength,
       Audio.ctx.sampleRate,
       AUDIO_FRAMES,
-      CGB_COLOR_CURVE
+      CGB_COLOR_CURVE,
     );
     if (this.e == 0) {
       throw new Error("Invalid ROM.");
@@ -222,7 +222,7 @@ class Emulator {
     const buffer = makeWasmBuffer(
       this.module,
       this.module._get_file_data_ptr(fileDataPtr),
-      this.module._get_file_data_size(fileDataPtr)
+      this.module._get_file_data_size(fileDataPtr),
     );
     const result = cb(fileDataPtr, buffer);
     this.module._file_data_delete(fileDataPtr);
@@ -887,7 +887,7 @@ class Gamepad {
       // Start polling the gamepad for input
       this.gp.timerID = setInterval(
         () => this.update(),
-        GAMEPAD_POLLING_INTERVAL
+        GAMEPAD_POLLING_INTERVAL,
       );
     }
   }
@@ -957,7 +957,7 @@ class Gamepad {
     // When a gamepad disconnects, shut down polling for input
     window.addEventListener(
       "gamepaddisconnected",
-      this.boundGamepadDisconnected
+      this.boundGamepadDisconnected,
     );
   }
 
@@ -967,7 +967,7 @@ class Gamepad {
     window.removeEventListener("gamepadconnected", this.boundGamepadConnected);
     window.removeEventListener(
       "gamepaddisconnected",
-      this.boundGamepadDisconnected
+      this.boundGamepadDisconnected,
     );
   }
 }
@@ -979,7 +979,7 @@ class Audio {
     this.buffer = makeWasmBuffer(
       this.module,
       this.module._get_audio_buffer_ptr(e),
-      this.module._get_audio_buffer_capacity(e)
+      this.module._get_audio_buffer_capacity(e),
     );
     this.startSec = 0;
     this.resume();
@@ -1030,7 +1030,7 @@ class Audio {
           this.startSec.toFixed(2) +
           " < " +
           nowSec.toFixed(2) +
-          ")"
+          ")",
       );
       this.startSec = nowPlusLatency;
     }
@@ -1072,12 +1072,12 @@ class Video {
     this.buffer = makeWasmBuffer(
       this.module,
       this.module._get_frame_buffer_ptr(e),
-      this.module._get_frame_buffer_size(e)
+      this.module._get_frame_buffer_size(e),
     );
     this.sgbBuffer = makeWasmBuffer(
       this.module,
       this.module._get_sgb_frame_buffer_ptr(e),
-      this.module._get_sgb_frame_buffer_size(e)
+      this.module._get_sgb_frame_buffer_size(e),
     );
   }
 
@@ -1096,7 +1096,7 @@ class Canvas2DRenderer {
     this.imageData = this.ctx.createImageData(SCREEN_WIDTH, SCREEN_HEIGHT);
     this.sgbImageData = this.ctx.createImageData(
       SGB_SCREEN_WIDTH,
-      SGB_SCREEN_HEIGHT
+      SGB_SCREEN_HEIGHT,
     );
 
     this.overlayCanvas = document.createElement("canvas");
@@ -1148,7 +1148,7 @@ class WebGLRenderer {
         void main(void) {
           gl_Position = vec4(aPos, 0.0, 1.0);
           vTexCoord = aTexCoord;
-        }`
+        }`,
     );
     const fragmentShader = compileShader(
       gl.FRAGMENT_SHADER,
@@ -1156,7 +1156,7 @@ class WebGLRenderer {
         uniform sampler2D uSampler;
         void main(void) {
           gl_FragColor = texture2D(uSampler, vTexCoord);
-        }`
+        }`,
     );
 
     const program = gl.createProgram();
@@ -1265,7 +1265,7 @@ class WebGLRenderer {
       0,
       gl.RGBA,
       gl.UNSIGNED_BYTE,
-      null
+      null,
     );
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
@@ -1284,7 +1284,7 @@ class WebGLRenderer {
       SCREEN_HEIGHT,
       gl.RGBA,
       gl.UNSIGNED_BYTE,
-      buffer
+      buffer,
     );
 
     gl.bindTexture(gl.TEXTURE_2D, this.sgbFbTexture);
@@ -1297,7 +1297,7 @@ class WebGLRenderer {
       SGB_SCREEN_HEIGHT,
       gl.RGBA,
       gl.UNSIGNED_BYTE,
-      sgbBuffer
+      sgbBuffer,
     );
   }
 
@@ -1332,7 +1332,7 @@ class Rewind {
     this.bufferPtr = this.module._rewind_new_simple(
       e,
       REWIND_FRAMES_PER_BASE_STATE,
-      REWIND_BUFFER_CAPACITY
+      REWIND_BUFFER_CAPACITY,
     );
     this.module._emulator_set_default_joypad_callback(e, this.joypadBufferPtr);
   }
@@ -1365,7 +1365,7 @@ class Rewind {
     this.statePtr = this.module._rewind_begin(
       this.e,
       this.bufferPtr,
-      this.joypadBufferPtr
+      this.joypadBufferPtr,
     );
   }
 
@@ -1380,7 +1380,7 @@ class Rewind {
     if (!this.isRewinding) return;
     this.module._emulator_set_default_joypad_callback(
       this.e,
-      this.joypadBufferPtr
+      this.joypadBufferPtr,
     );
     this.module._rewind_end(this.statePtr);
     this.statePtr = 0;

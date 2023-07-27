0d2e2b5cb48c75dfef7751e04342c4a894be8e2e
Sam Elliott <samde@microsoft.com>
2023-07-27 00:37:46 -0400

    qol fixes

---
+++ app/beluga/page.tsx
@@ -0,0 +1,12 @@
+function Page() {
+  return (
+    <>
+      <iframe
+        src="https://sams-awesome-space.nyc3.digitaloceanspaces.com/index.html"
+        className="h-screen w-screen"
+      />
+    </>
+  );
+}
+
+export default Page;
--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/beluga",
-        destination:
-          "https://sams-awesome-space.nyc3.digitaloceanspaces.com/index.html",
-      },
-      {
-        source: "/beluga/:slug",
-        destination:
-          "https://sams-awesome-space.nyc3.digitaloceanspaces.com/index.html/:slug",
-      },
-    ];
-  },
 };

0d5a695f3c95f0a53d09634d60f20bbea508c5af
Sam Elliott De La Torre Babá <sdelatorrebaba@gmail.com>
2023-09-29 17:14:05 -0400

    Add kiki (#11)
    * add kiki
    
    * update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import "./page.css";
 
 function Home() {
   return (
     <div className="home-container">
       <h1>Hello, world.</h1>
+      <Image
+        src="/kiki-fly.svg"
+        alt="A flying Kiki"
+        className="fixed bottom-2 right-2 w-16"
+        width={500}
+        height={500}
+      />
     </div>
   );
 }

80515a6408f71988a7a9af49b7a688cd63783cc0
Sam Elliott <sdelatorrebaba@gmail.com>
2024-06-20 00:36:32 -0400

    Update email address format in about me page

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -14,9 +14,9 @@ function Page() {
       </ul>
       <br />
       <p className="text-xl text-black mb-2">
-        You can reach me at:{" "}
-        <a href="mailto:me@sdelatorrebaba.com">
-          You can reach me at: me@sdelatorrebaba.com
+        You can reach me at: You can reach me at:{" "}
+        <a href="mailto:me@sdelatorrebaba.com" className="text-blue-400">
+          me@sdelatorrebaba.com
         </a>
       </p>
     </div>

0ff98a116afb995c5720aa83d7b14636456c70bb
Sam Elliott <sdelatorrebaba@gmail.com>
2025-07-25 21:05:42 -0400

    refactor: remove unused Navbar import from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
-import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 export const metadata = {
